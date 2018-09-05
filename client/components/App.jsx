06bc6d908e36cad1be44095eb5d8e042914888b8
lianna-blanca <lianna.blanca@gmail.com>
2018-09-05 12:20:29 +1200

    Add pictures

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
+const request = require('superagent')
 
-const App = () => {
+const apiEndpointBase = '/v1/'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this. state = {
+      natural: []
+    }
+
+    this.getList = this.getList.bind(this)
+  }
+
+componentDidMount() {
+  this.getList()
+}
+
+getList() {
+  return request.get(apiEndpointBase + 'pictures')
+    .then(res => {
+      this.setState({
+        natural: res.body
+
+      })
+    })
+}
+
+
+  render () {
   return (
-    <h1>React development has begun!</h1>
-  )
+    <div onLoad={(() => this.getList())}>
+      <h1>Natural Pictures</h1>
+      <ul>
+        {this.state.natural.map(something => {
+          return <li>
+            {something.identifier}
+            <img src={"https://epic.gsfc.nasa.gov/archive/natural/2018/09/02/jpg/" + something.image + ".jpg"} />
+          </li>
+        })}
+      </ul>
+    </div>
+    )
+  }
 }
 
 export default App
--- server/server.js
+++ server/server.js
@@ -1,9 +1,29 @@
 const path = require('path')
 const express = require('express')
-
+const request = require('superagent')
 const server = express()
+
+const apiEndpointBase = "https://epic.gsfc.nasa.gov/api/natural/"
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
+
+server.get("/v1/pictures", (req, res) => {
+  // res.send("hiya ????")
+  request.get(apiEndpointBase + 'pictures')
+  .then(ApiRes => {
+    res.json(ApiRes.body)
+  })
+})
+
+// server.get("/v1/natural/:id", (req, res) => {
+//   let thisID = req.params.id
+//   console.log(thisID)
+//   request.get("https://epic.gsfc.nasa.gov/api/natural/" + thisID)
+//   .then(ApiRes => {
+//     res.json(ApiRes.body)
+//   })
+// })
+
 
 module.exports = server
